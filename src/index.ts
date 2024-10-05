9807fabae07f9950ced1069bee36d0a32c127e08
Gabriel Malosto <professional@gabdumal.com>
2024-10-05 11:19:38 -0300

    Set up a relay node

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import { createLibp2p } from "libp2p";
 import { webSockets } from "@libp2p/websockets";
 import { noise } from "@chainsafe/libp2p-noise";
 import { yamux } from "@chainsafe/libp2p-yamux";
+import { bootstrap } from "@libp2p/bootstrap";
+
+// Known peers addresses
+const bootstrapMultiaddrs = [
+  "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+  "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+];
 
 const node = await createLibp2p({
   start: false,
@@ -11,15 +18,32 @@ const node = await createLibp2p({
   transports: [webSockets()],
   connectionEncrypters: [noise()],
   streamMuxers: [yamux()],
+  peerDiscovery: [
+    bootstrap({
+      list: bootstrapMultiaddrs, // Provide array of multiaddrs
+    }),
+  ],
 });
 
-// start libp2p
+/// Listeners
+
+node.addEventListener("peer:discovery", (evt) => {
+  console.log("Discovered %s", evt.detail.id.toString()); // Log discovered peer
+});
+
+node.addEventListener("peer:connect", (evt) => {
+  console.log("Connected to %s", evt.detail.toString()); // Log connected peer
+});
+
+/// Lifecycle
+
+// Start libp2p
 await node.start();
 console.log("libp2p has started");
 
 const listenAddress = node.getMultiaddrs();
 console.log("libp2p is listening on the following addresses: ", listenAddress);
 
-// stop libp2p
+// Stop libp2p
 await node.stop();
 console.log("libp2p has stopped");
