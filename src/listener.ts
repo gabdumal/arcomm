704f927c099ac1d1a2f9fbeea5c10e385150cb43
Gabriel Malosto <professional@gabdumal.com>
2024-10-05 11:32:19 -0300

    Set up a listener node

---
+++ src/listener.ts
@@ -0,0 +1,41 @@
+import { noise } from "@chainsafe/libp2p-noise";
+import { yamux } from "@chainsafe/libp2p-yamux";
+import { circuitRelayTransport } from "@libp2p/circuit-relay-v2";
+import { identify } from "@libp2p/identify";
+import { webSockets } from "@libp2p/websockets";
+import { multiaddr } from "@multiformats/multiaddr";
+import { createLibp2p } from "libp2p";
+
+const relayAddrArg = process.argv[2];
+if (!relayAddrArg) {
+  throw new Error("The relay address needs to be specified as a parameter");
+}
+const relayAddr = multiaddr(relayAddrArg);
+
+const node = await createLibp2p({
+  transports: [
+    webSockets(),
+    circuitRelayTransport({
+      discoverRelays: 2,
+    }),
+  ],
+  connectionEncrypters: [noise()],
+  streamMuxers: [yamux()],
+  services: {
+    identify: identify(),
+  },
+});
+
+console.log(`Node started with id ${node.peerId.toString()}`);
+
+const conn = await node.dial(relayAddr);
+
+console.log(`Connected to the relay ${conn.remotePeer.toString()}`);
+
+// Wait for connection and relay to be bind for the example purpose
+node.addEventListener("self:peer:update", (evt) => {
+  // Updated self multiaddrs?
+  console.log(
+    `Advertising with a relay address of ${node.getMultiaddrs()[0].toString()}`
+  );
+});
--- src/relay.ts
+++ src/relay.ts
@@ -7,7 +7,7 @@ import { createLibp2p } from "libp2p";
 
 const node = await createLibp2p({
   addresses: {
-    listen: ["/ip4/0.0.0.0/tcp/0/ws"],
+    listen: ["/ip4/127.0.0.1/tcp/8082/ws"],
     // TODO check "What is next?" section
     // announce: ['/dns4/auto-relay.libp2p.io/tcp/443/wss/p2p/QmWDn2LY8nannvSWJzruUYoLZ4vV83vfCBwd8DipvdgQc3']
   },
@@ -22,4 +22,4 @@ const node = await createLibp2p({
 
 console.log(`Node started with id ${node.peerId.toString()}`);
 console.log("Listening on:");
-node.getMultiaddrs().forEach((ma) => console.log(ma.toString()));
+node.getMultiaddrs().forEach((ma) => console.log("  " + ma.toString()));
